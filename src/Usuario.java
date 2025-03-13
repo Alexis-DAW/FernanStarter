2271963efd89b33a8db5c744c15ca05ad560ea5b
Alexis <isalexis.design@gmail.com>
2025-03-13 13:42:18 +0100

    Clase Usuario con constructor

--- src/Usuario.java
+++ src/Usuario.java
@@ -4,6 +4,11 @@ public abstract class Usuario {
     private String contrasena;
     private String correo;
 
+    public Usuario (String nombre, String contrasena, String correo){
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.correo = correo;
+    }
 
 
 }
