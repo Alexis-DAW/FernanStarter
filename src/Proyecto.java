cb9c92d5d57adaa556db2d0e516a635469d84208
Alexis <isalexis.design@gmail.com>
2025-03-13 14:43:14 +0100

    1

---
+++ src/Proyecto.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+
+public class Proyecto {
+
+    private static int contadorProyectos;
+    private int id;
+    private String nombre;
+    private String descripcion;
+    // enum categoria
+    private float cantidadNecesaria;
+    private float cantidadFinanciada;
+    LocalDate fechaInicio;
+    LocalDate fechaFin;
+
+
+
+}
